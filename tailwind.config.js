6e61886e30c34ef9cd5da03696db5ac57acea53f
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-02-22 13:42:35 +0100

    postcss + autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
       // Color names from https://www.color-name.com/
