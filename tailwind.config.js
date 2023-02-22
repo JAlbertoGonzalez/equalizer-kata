cba82072cf3ef34582d49a507485cb6885322702
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-02-22 15:05:57 +0100

    tailwind config + svg assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,34 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      // Color names from https://www.color-name.com/
-      'eerie-black': '#191826',
-      'skyblue': '#66E2DC',
-      'outrageous-orange': '#53d9fa',
-      'rajah': '#FFB964',
-      'snow': '#FCFAF9'
+    extend: {
+      fontSize: {
+        'heading-1': '88px',
+        'heading-2': '40px',
+      },
+      colors: {
+        // Color names from https://www.color-name.com/
+        'eerie-black': '#191826',
+        'skyblue': '#66E2DC',
+        'outrageous-orange': '#FA7453',
+        'rajah': '#FFB964',
+        'snow': '#FCFAF9'
+      },
+      fontFamily: {
+        ibm: ["IBM Plex Sans"]
+      },
+
     },
-    fontFamily: {
-      ibm: ["IBM Plex Sans"]
-    },
-    extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase, config }) => {
+      addBase({
+        'h1': {
+        }
+      })
+    })
+  ],
 }
