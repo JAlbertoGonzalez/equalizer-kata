cffeb8334f62d95013e39858e2a862905c1b735c
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2023-02-22 12:52:28 +0100

    ibm font + default colors scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: [],
   theme: {
+    colors: {
+      // Color names from https://www.color-name.com/
+      'eerie-black': '#191826',
+      'skyblue': '#66E2DC',
+      'outrageous-orange': '#53d9fa',
+      'rajah': '#FFB964',
+      'snow': '#FCFAF9'
+    },
+    fontFamily: {
+      ibm: ["IBM Plex Sans"]
+    },
     extend: {},
   },
   plugins: [],
